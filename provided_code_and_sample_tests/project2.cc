7d0fe27bd144e3ffd0fb57da250eb14151ca4d3a
sm11t <adatta18@asu.edu>
2025-03-24 17:30:52 -0700

    parser works

--- provided_code_and_sample_tests/project2.cc
+++ provided_code_and_sample_tests/project2.cc
@@ -1,100 +1,123 @@
-/*
- * Copyright (C) Mohsen Zohrevandi, 2017
- *               Rida Bazzi 2019
- * Do not share this file with anyone
- */
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <vector>
+#include <string>
 #include "lexer.h"
 
 using namespace std;
 
-// read grammar
+struct Parsing_Rules {
+    string lhs;
+    vector<string> rhs;
+};
+
+vector<Parsing_Rules> grammarRules;
+LexicalAnalyzer lex;
+Token currentToken;
+
+void getNextToken() {
+    currentToken = lex.GetToken();
+}
+
+void eat(TokenType expected) {
+    if (currentToken.token_type != expected) {
+        cout << "SYNTAX ERROR !!!!!!!!!!!!!!" << endl;
+        exit(1);
+    }
+    getNextToken();
+}
+
+vector<string> id_list();
+vector< vector<string> > right_hand_side();
+void rule();
+void grammar();
+
+void grammar() {
+    while (currentToken.token_type == ID){
+        string lhs = currentToken.lexeme;
+        eat(ID);
+        eat(ARROW);
+        vector< vector<string> > alternatives = right_hand_side();
+        eat(STAR);
+        for (auto &alt : alternatives) {
+            Parsing_Rules pr;
+            pr.lhs = lhs;
+            pr.rhs = alt;
+            grammarRules.push_back(pr);
+        }
+    }
+
+    if (currentToken.token_type != HASH) {
+        cout << "SYNTAX ERROR !!!!!!!!!!!!!!" << endl;
+        exit(1);
+    }
+    eat(HASH);
+}
+
+vector< vector<string> > right_hand_side() {
+    vector< vector<string> > alternatives;
+    vector<string> alt = id_list();
+    alternatives.push_back(alt);
+    while (currentToken.token_type == OR) {
+        eat(OR);
+        alt = id_list();
+        alternatives.push_back(alt);
+    }
+    return alternatives;
+}
+
+vector<string> id_list() {
+    vector<string> symbols;
+    while (currentToken.token_type == ID) {
+        symbols.push_back(currentToken.lexeme);
+        eat(ID);
+    }
+    return symbols;
+}
+
 void ReadGrammar() {
+    getNextToken();
+    grammar();
 }
 
-/* 
- * Task 1: 
- * Printing the terminals, then nonterminals of grammar in appearing order
- * output is one line, and all names are space delineated
-*/
-void Task1()
-{
+void Task1() {
 }
 
-/*
- * Task 2:
- * Print out nullable set of the grammar in specified format.
-*/
-void Task2()
-{
+void Task2() {
 }
 
-// Task 3: FIRST sets
-void Task3()
-{
+void Task3() {
 }
 
-// Task 4: FOLLOW sets
-void Task4()
-{
+void Task4() {
 }
 
-// Task 5: left factoring
-void Task5()
-{
+void Task5() {
 }
 
-// Task 6: eliminate left recursion
-void Task6()
-{
+void Task6() {
 }
     
 int main (int argc, char* argv[])
 {
     int task;
-
-    if (argc < 2)
-    {
+    if (argc < 2) {
         cout << "Error: missing argument\n";
         return 1;
     }
-
-    /*
-       Note that by convention argv[0] is the name of your executable,
-       and the first argument to your program is stored in argv[1]
-     */
-
     task = atoi(argv[1]);
-    
-    ReadGrammar();  // Reads the input grammar from standard input
-                    // and represent it internally in data structures
-                    // ad described in project 2 presentation file
-
+    ReadGrammar();
     switch (task) {
-        case 1: Task1();
-            break;
-
-        case 2: Task2();
-            break;
-
-        case 3: Task3();
-            break;
-
-        case 4: Task4();
-            break;
-
-        case 5: Task5();
-            break;
-        
-        case 6: Task6();
-            break;
-
+        case 1: Task1(); break;
+        case 2: Task2(); break;
+        case 3: Task3(); break;
+        case 4: Task4(); break;
+        case 5: Task5(); break;
+        case 6: Task6(); break;
         default:
             cout << "Error: unrecognized task number " << task << "\n";
             break;
     }
     return 0;
 }
-
